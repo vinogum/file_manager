4c41381c3e4c06358a7c2d46d2a24c3a375be23d
nikolay_vynohradov <vinogum@gmail.com>
2025-03-16 17:10:27 +0200

    Connect rest_framework.authtoken and stratapp accounts

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_framework.authtoken', # Token Authentication
     "files",
-    "api"
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
