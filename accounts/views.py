5f67431607f0f90a0c9a48963d1796f11527da1c
nikolay_vynohradov <vinogum@gmail.com>
2025-03-16 19:53:56 +0200

    Add RegisterAPIView and LoginAPIView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response, status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+def RegisterAPIView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        if not (username and password):
+            return Response(data={"message": "Some fields are not filled in!"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.filter(username=username)
+        if user.exists():
+            return Response(data={"message": "Such user already exists!"}, status=status.HTTP_409_CONFLICT)
+        
+        user = User.objects.create_user(username=username, password=password)
+        token, _ = Token.objects.create(user=user)
+        
+        return Response(data={"token": token.key}, status=status.HTTP_201_CREATED)
+
+
+def LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        if not (username and password):
+            return Response(data={"message": "Some fields are not filled in!"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.filter(username=username)
+        if not user.exists():
+            return Response(data={"message": "Such user does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        user = user.first()
+        if not user.check_password(password):
+            return Response(data={"message": "Invalid password!"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        token, _ = Token.objects.get(user=user)
+        return Response(data={"token": token.key}, status=status.HTTP_200_OK)
