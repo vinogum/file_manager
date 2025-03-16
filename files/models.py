1941dba4923a6eed6c82027d806054ae6559c1b2
nikolay_vynohradov <vinogum@gmail.com>
2025-03-16 15:16:20 +0200

    Add initial models for files/models

--- files/models.py
+++ files/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def upload_to(instance, filename):
+    pass
+
+class File(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    class Meta:
+        unique_together = ("user", "name")
+
+class Version(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    version = models.PositiveIntegerField()
+    file_data = models.FileField(upload_to=upload_to)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("file", "version")
