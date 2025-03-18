fa117feeed28cb9e9c238bf83691becb1aef4c0d
nikolay_vynohradov <vinogum@gmail.com>
2025-03-18 13:00:16 +0200

    Fix files/models.py. Change files/views.py and files/serializer.py

--- files/views.py
+++ files/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import File
-from .serializer import FileSerializer
-from rest_framework.permissions import IsAuthenticated
 
 
 class FileViewSet(viewsets.ModelViewSet):
-    queryset = File.objects.all()
-    serializer_class = FileSerializer
-    permission_classes = [IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        pass
-
+    pass
 
 def get_file(request):
     if request.method == "GET":
