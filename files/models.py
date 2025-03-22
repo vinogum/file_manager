8cab139dc821d55d046259d976fc424be20b167b
nikolay_vynohradov <vinogum@gmail.com>
2025-03-22 11:27:01 +0200

    Add hash function

--- files/models.py
+++ files/models.py
@@ -1,8 +1,34 @@
+from django.core.files import uploadedfile
+from django.core import files
 from django.db import models
 from django.contrib.auth.models import User
+import hashlib
+import os
+
+
+def get_file_hash(file):
+    if isinstance(file, uploadedfile.InMemoryUploadedFile):
+        binary_data = file.read() # Get binary content of the file
+        hash_object = hashlib.sha256(binary_data) # Get the hash of binary content as hash_object
+        return hash_object.hexdigest() # Convert the hash_object to readable format (string)
+    
+    elif isinstance(file, files.File):
+        binary_data = file.read()
+        hash_object = hashlib.sha256(binary_data)
+        return hash_object.hexdigest()
+
+    elif isinstance(file, str) and os.path.isfile(file):
+        with open(file, "rb") as filestream:
+            binary_data = filestream.read()
+            hash_object = hashlib.sha256(binary_data)
+            return hash_object.hexdigest()
+    else:
+        return None
+
 
 def upload_to(instance, filename):
-    pass
+    user_folder = f"user_{instance.file.user.id}/{filename}"
+    return user_folder
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
